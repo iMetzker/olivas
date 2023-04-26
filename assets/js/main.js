15221aeee931bfb3e20176da605b7b069fa28b90
Ivny Metzker <ivnymetzker@gmail.com>
2023-04-25 21:19:10 -0300

    Style - News
    Estilizando o carrosel de imagens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,25 @@
-// POST CLICK SUMMARY
+// CARROUSEL - NEWS
 
-function aboutPostOn () {
-    const target = document.querySelector('.other-posts-summary');
-    target.classList.toggle('active');
+let image = document.querySelector('.s1');
+let radio1 = document.querySelector('#radio1');
+let radio2 = document.querySelector('#radio2');
+let radio3 = document.querySelector('#radio3');
+let radio4 = document.querySelector('#radio4');
+let radio5 = document.querySelector('#radio5');
+let radio6 = document.querySelector('#radio6');
+
+function move () {
+    if (radio1.checked) {
+        image.style.marginLeft = '0%';
+    } else if (radio2.checked) {
+        image.style.marginLeft = '-17%';
+    } else if (radio3.checked) {
+        image.style.marginLeft = '-34%';
+    } else if (radio4.checked) {
+        image.style.marginLeft = '-51%';
+    } else if (radio5.checked) {
+        image.style.marginLeft = '-68%';
+    } else if (radio6.checked) {
+        image.style.marginLeft = '-85%';
+    }
 }
